b1b1b2929eb8c416f8b70cf29a163623a6d7b5f2
psc <cowork1263@gmail.com>
2020-10-25 13:23:49 +0900

    1025__2

---
+++ seongcheol/test_1025.py
@@ -0,0 +1,50 @@
+import mahotas as mh
+import numpy as np
+from matplotlib import pyplot as plt
+from glob import glob
+from scipy.spatial import distance
+from sklearn.preprocessing import StandardScaler
+# %matplotlib inline
+
+
+path = "./imgimg/*.jpg"
+
+def feature_extraction(path):
+
+    images = glob(path)
+    features = []
+    # labels = []
+    for i in images:
+    #    labels.append(i[14:-len('.jpg')])
+        i = mh.imread(i)
+        i = mh.colors.rgb2grey(i, dtype = np.uint8)
+        features.append(mh.features.haralick(i).ravel())
+    features = np.array(features)
+    sc = StandardScaler()
+    features = sc.fit_transform(features)
+    dists = distance.squareform(distance.pdist(features))
+
+    return images, dists
+
+
+def select_all(n, dists, images):
+    dists_idx_sorted = dists[n].argsort()                                      
+    similar_list = []
+    for i in dists_idx_sorted:       
+        if dists[n][i] < 3.5:
+            similar_list.append(images[i])
+    return similar_list
+
+
+
+#-------------------------------------------------
+# dists = feature_extraction(path)[1]
+# images = feature_extraction(path)[0]
+# print(select_all(5,dists,images))
+
+if len(select_all(5,feature_extraction(path)[1],feature_extraction(path)[0])) ==1:
+    print('X')
+else:
+    print(select_all(5,feature_extraction(path)[1],feature_extraction(path)[0]))
+
+
