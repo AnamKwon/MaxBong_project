a40d6dfc28e44a8234f5dce03dd4c15ab9976747
psc <cowork1263@gmail.com>
2020-10-25 15:23:27 +0900

    1025

--- seongcheol/test_1025.py
+++ seongcheol/test_1025.py
@@ -1,5 +1,7 @@
+import os
 import mahotas as mh
 import numpy as np
+import cv2
 from matplotlib import pyplot as plt
 from glob import glob
 from scipy.spatial import distance
@@ -36,5 +38,58 @@ def select_all(n, dists, images):
     return similar_list
 
 
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+
+def size_normal(path):
+
+    g_path = glob.glob(path)
+    cv_img = []
+
+    for i in g_path:
+        n = cv2.imread(i)
+        cv_img.append(n)
+
+    # print(cv_img[0].shape[0])
 
 
+    m_width = 0            
+    m_height = 0
+    
+    for j in range(len(cv_img)):
+        if m_width < cv_img[j].shape[0]:
+            m_width = cv_img[j].shape[0]
+        elif m_height < cv_img[j].shape[1]:
+            m_height = cv_img[j].shape[1]
+    
+    return cv_img, m_width, m_height
+
+
+
+def r_size(img_list, w, h):
+
+    re_size = [] 
+    for i in img_list:
+        re_size.append(cv2.resize(i, dsize = (w,h), interpolation=cv2.INTER_AREA ))
+    return re_size
+
+
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+
+# os.getcwd()  path
+
+def imgwrite(slist,f_name):
+    os.mkdir(str(f_name))
+    count = 0
+    for i in slist:
+        
+        imgfile = i
+        img = cv2.imread(imgfile, cv2.IMREAD_COLOR)
+        cv2.imwrite(str(f_name)+'/' + str(count)+'.jpg',img)
+        count = count + 1
+
+imgwrite(select_all(5,feature_extraction(path)[1],feature_extraction(path)[0]),'test_sim_img')
+
+
+        
