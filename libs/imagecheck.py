b4a4c37836750eb3d1788f6b760fe38ba73f7f05
unknown <kan9188@gmail.com>
2020-11-03 17:22:09 +0900

    Ui update and similar img check

---
+++ libs/imagecheck.py
@@ -0,0 +1,103 @@
+import os
+import mahotas as mh
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from glob import glob
+from scipy.spatial import distance
+from sklearn.preprocessing import StandardScaler
+# %matplotlib inline
+
+
+path = "./imgimg/*.jpg"
+
+def feature_extraction(path):
+
+    images = glob(path)
+    features = []
+    # labels.txt = []
+    for i in images:
+    #    labels.txt.append(i[14:-len('.jpg')])
+        i = mh.imread(i)
+        i = mh.colors.rgb2grey(i, dtype = np.uint8)
+        features.append(mh.features.haralick(i).ravel())
+    features = np.array(features)
+    sc = StandardScaler()
+    features = sc.fit_transform(features)
+    dists = distance.squareform(distance.pdist(features))
+
+    return images, dists
+# print(len(feature_extraction(path)[0]))
+
+def select_all(n, dists, images):
+    dists_idx_sorted = dists[n].argsort()                                      
+    similar_list = []
+    for i in dists_idx_sorted:       
+        if dists[n][i] < 3.5:
+            similar_list.append(images[i])
+    return similar_list
+
+
+
+img_dict = dict()
+for i in range(len(feature_extraction(path)[0])):
+    img_dict[str(feature_extraction(path)[0][i])] = select_all(i,feature_extraction(path)[1],feature_extraction(path)[0])
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+print(img_dict)
+
+
+
+def size_normal(path):
+
+    g_path = glob.glob(path)
+    cv_img = []
+
+    for i in g_path:
+        n = cv2.imread(i)
+        cv_img.append(n)
+
+    # print(cv_img[0].shape[0])
+
+
+    m_width = 0            
+    m_height = 0
+    
+    for j in range(len(cv_img)):
+        if m_width < cv_img[j].shape[0]:
+            m_width = cv_img[j].shape[0]
+        elif m_height < cv_img[j].shape[1]:
+            m_height = cv_img[j].shape[1]
+    
+    return cv_img, m_width, m_height
+
+
+
+def r_size(img_list, w, h):
+
+    re_size = [] 
+    for i in img_list:
+        re_size.append(cv2.resize(i, dsize = (w,h), interpolation=cv2.INTER_AREA ))
+    return re_size
+
+
+#---------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+
+# os.getcwd()  path
+
+def imgwrite(slist,f_name):
+    os.mkdir(str(f_name))
+    count = 0
+    for i in slist:
+        
+        imgfile = i
+        img = cv2.imread(imgfile, cv2.IMREAD_COLOR)
+        cv2.imwrite(str(f_name)+'/' + str(count)+'.jpg',img)
+        count = count + 1
+
+# imgwrite(select_all(5,feature_extraction(path)[1],feature_extraction(path)[0]),'test_sim_img')
+
+
+
+
--- main_v1.py
+++ main_v1.py
@@ -1,17 +1,34 @@
 # coding: utf-8
-
 import sys
-import os
-import glob
-from Data_type import *
+from libs.Data_type import *
+from libs.convType import *
+from libs.imagecheck import *
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import uic
+from PyQt5 import QtCore
 from PyQt5.QtCore import pyqtSlot
 
 
-class Form(QtWidgets.QMainWindow):
+class test(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        QtWidgets.QDialog.__init__(self, parent)
+        self.ui = uic.loadUi("C:/Users/kan91/Desktop/QT/ui.ui", self)
 
+    @pyqtSlot()
+    def slot_1st(self):
+        self.ui.label.setText("첫번째 버튼")
+
+    @pyqtSlot()
+    def slot_2nd(self):
+        self.ui.label.setText("두번째 버튼")
+
+    @pyqtSlot()
+    def slot_3rd(self):
+        self.ui.label.setText("세번째 버튼")
+
+class Form(QtWidgets.QMainWindow):
+    switch_window = QtCore.pyqtSignal(str)
     def __init__(self, parent=None):
         QtWidgets.QMainWindow.__init__(self, parent)
         self.ui = uic.loadUi("main_V2.ui", self)
@@ -24,10 +41,40 @@ class Form(QtWidgets.QMainWindow):
         self.ui.actionCOCO.triggered.connect(lambda: self.Open('COCO'))
         self.ui.actionVOC.triggered.connect(lambda: self.Open('VOC'))
         self.ui.actionYOLO.triggered.connect(lambda: self.Open('YOLO'))
-
+        self.ui.actionImage_duplicate_check_2.triggered.connect(self.Img_check)
+        self.ui.actionImage_duplicate_check_2.triggered.connect(self.Img_check2)
+        self.ui.actionView_Duplicate_list_2.triggered.connect(self.View_D_Image)
         self.ui.File_L.clicked.connect(self.Img_view)
         self.startpos = None
         self.endpos = None
+        self.on_similar = False
+
+    def Img_check(self):
+        self.on_similar = True
+        print(self.file_path)
+        self.ui.right_layout.setHidden(not self.ui.right_layout.isHidden())
+        print(2)
+        self.ui.actionView_Duplicate_list_2.setChecked(True)
+        print(3)
+        try :
+            files = list(self.dataset.files_dict.keys())
+        except :
+            files = self.file_list
+        print(files)
+        self.similar_list = check_img(files, self.file_path)
+        print(5)
+
+
+
+    def Img_check2(self):
+        pass
+
+    def View_D_Image(self):
+        if self.ui.right_layout.isHidden() :
+            self.ui.actionView_Duplicate_list_2.setChecked(True)
+        else :
+            self.ui.actionView_Duplicate_list_2.setChecked(False)
+        self.ui.right_layout.setHidden(not self.ui.right_layout.isHidden())
 
     def mouseReleaseEvent(self, e):  # e ; QMouseEvent
         print('BUTTON RELEASE')
@@ -35,35 +82,38 @@ class Form(QtWidgets.QMainWindow):
         print(self.startpos)
         print(self.endpos)
         print(self.ui.Image_V.size())
-        print(self.ui.centralwidget.childAt(self.ui.Image_V.pos()).pos())
-        print(self.ui.scrollArea.pos())
+        print(self.ui.scrollAreaWidgetContents_2.childAt(self.ui.Image_V.pos()).pos())
+        print(self.ui.centralwidget.childAt(self.ui.widget.pos()).y())
 
+
+    def resizeEvent(self, a) :
+        print(a.size())
+        print(self.ui.File_L.width())
 
     def mousePressEvent(self, e):  # e ; QMouseEvent
         print('BUTTON PRESS')
+        #print(self.img_pos_view)
         self.startpos = (e.x(), e.y())
 
     def mouseMoveEvent(self, e):
 
-        x = e.x() - 250
+        x = e.x() - 251
         y = e.y() - 26
 
         text = 'x: {0}, y: {1}'.format(x, y)
         print(self.Image_V.geometry())
         print(text)
 
+    @pyqtSlot()
     def Img_view(self):
+        self.ui.Img_D_V_L.clear()
         img = QtGui.QPixmap()
-        img.load(self.ui.File_L.currentItem().text())
-        print(self.ui.Image_V.width()/img.width())
-        print(self.ui.Image_V.height()/img.height())
-        if self.ui.Image_V.width()/img.width() > self.ui.Image_V.height()/img.height() :
-            x = self.ui.Image_V.height()/img.height()
-        else :
-            x = self.ui.Image_V.width()/img.width()
-        img = img.scaled(int(img.width()), int(img.height()))
+        img_file = self.ui.File_L.currentItem().text()
+        img.load(img_file)
         self.ui.Image_V.setFixedSize(img.size())
         self.ui.Image_V.setPixmap(img)
+        if self.on_similar :
+            self.ui.Img_D_V_L.addItems(self.similar_list[img_file])
 
 
     @pyqtSlot()
@@ -75,33 +125,40 @@ class Form(QtWidgets.QMainWindow):
         elif val in [2 , 'VOC' , 'YOLO']:
             options = {2:['jpg','png','bmp','jpeg'],'VOC':['xml'],'YOLO':['txt']}[val]
             files_path = QtWidgets.QFileDialog.getExistingDirectory()
+            if files_path == '' :
+                return
             os.chdir(files_path)
             if val == 2 :
                 for i in options :
-                    self.file_list += glob.glob(f'*.{i}')
+                    self.file_list += glob(f'*.{i}')
             elif val == 'VOC' :
-                xml_files = glob.glob('*.xml')
+                xml_files = glob('*.xml')
                 self.dataset = VOC(xml_files)
                 self.file_list = get_image(self.dataset)
 
             elif val == 'YOLO' :
                 label_file = QtWidgets.QFileDialog.getOpenFileName(filter="Labels (*.txt)",directory=files_path)[0]
-                txt_files = glob.glob(f'*.txt')
+                txt_files = glob(f'*.txt')
                 self.dataset = YOLO(txt_files, label_file)
                 self.file_list = get_image(self.dataset)
 
 
         elif val == 'COCO' :
             files_path = QtWidgets.QFileDialog.getOpenFileName(filter="MS COCO (*.json)")[0]
+            if files_path == '' :
+                return
             os.chdir(files_path.rsplit('/',1)[0])
             self.dataset = COCO(files_path)
             self.file_list = get_image(self.dataset)
+        self.file_path = files_path
         self.ui.File_L.addItems(self.file_list)
         self.ui.actionData_Processing.setEnabled(True)
         self.ui.actionData_Trans_Form.setEnabled(True)
         self.ui.actionImage_duplicate_check_2.setEnabled(True)
         self.ui.actionView_Duplicate_list_2.setEnabled(True)
         self.ui.actionView_Duplicate_image.setEnabled(True)
+        self.on_similar = False
+
 
 
 
@@ -113,4 +170,5 @@ if __name__ == '__main__':
 
 #QtWidgets.QListWidget.selectedItems()
 QtWidgets.QLabel.childAt().pos()
-QtWidgets.QMainWindow.childAt()
+QtWidgets.QFileDialog.getOpenFileNames()
+
